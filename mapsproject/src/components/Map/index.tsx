694600ef58f5afb7aa2fb2a769082433b30e6cb7
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-08 12:46:14 -0300

    Search input for MapsApi + styleInput

--- mapsproject/src/components/Map/index.tsx
+++ mapsproject/src/components/Map/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Styled from "./styles";
 
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
 
 const center = {
   lat: -23.551032,
@@ -9,14 +13,50 @@ const center = {
 };
 
 const Map = () => {
+  const [searchBox, setSearchBox] = useState<google.maps.places.SearchBox>();
+  const [map, setMap] = useState<google.maps.Map>();
+
+  //Referencias de mapas
+  const onLoad = (ref: google.maps.places.SearchBox) => {
+    setSearchBox(ref);
+  };
+
+  //carrega as referencia de mapa
+  const onMapLoad = (map: google.maps.Map) => {
+    setMap(map);
+  };
+
+  const onPlacesChanged = () => {
+    // pegamos lat e lng pra criar novo objeto de localização, apontando para o novo local e centralizando
+    const places = searchBox!.getPlaces();
+    const place = places![0];
+    const location = {
+      lat: place?.geometry?.location?.lat() || 0,
+      lng: place?.geometry?.location?.lng() || 0,
+    };
+
+    map?.panTo(location);
+  };
+
   return (
     <Styled.Container>
-      <LoadScript googleMapsApiKey="AIzaSyDqdDpwqMa3_psd_DvvOdkNdVuXAe8tzVQ">
+      <LoadScript
+        googleMapsApiKey="AIzaSyDqdDpwqMa3_psd_DvvOdkNdVuXAe8tzVQ"
+        libraries={["places"]}
+      >
         <GoogleMap
+          onLoad={onMapLoad}
           center={center}
           zoom={15}
           mapContainerStyle={{ width: "100%", height: "100%" }}
-        />
+        >
+          <StandaloneSearchBox
+            onLoad={onLoad} //pegamos as referencias atravez do onload e setamos no nosso input
+            onPlacesChanged={onPlacesChanged} //
+          >
+            <Styled.Input placeholder="Procure um parceiro Soon" />
+          </StandaloneSearchBox>
+        </GoogleMap>
       </LoadScript>
     </Styled.Container>
   );
--- mapsproject/src/components/Map/styles.ts
+++ mapsproject/src/components/Map/styles.ts
@@ -4,3 +4,17 @@ export const Container = styled.div`
   height: 100vh;
   width: 100vw;
 `;
+
+export const Input = styled.input`
+  border: 1px solid transparent;
+  width: 350px;
+  height: 32px;
+  padding: 0 12px;
+  border-radius: 4px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  font-size: 14px;
+  text-overflow: ellipsis;
+  position: absolute;
+  left: 50%;
+  margin-top: 50rem;
+`;
